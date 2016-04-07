3cffb7673e0bc0cf43476f243cfc85eb58ebdd45
zhong_s <hillwater.zju@gmail.com>
2016-04-08 02:19:05 +0900

    need docker login

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ IMAGE=hillwater/gomoku-engine:1.0.0
 
 # should set environment variable CLOUDAMQP_URL to rabbitmq url
 
+sudo docker login
+
 sudo docker run -e "CLOUDAMQP_URL=$CLOUDAMQP_URL" -d $IMAGE
