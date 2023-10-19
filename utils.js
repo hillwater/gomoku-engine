102ad427f556696ccf4f34b16264e77bf83b8df7
hillwater <hillwater.zju@gmail.com>
2023-10-20 03:03:07 +0800

    redis access avoid duplciate positions

--- utils.js
+++ utils.js
@@ -2,12 +2,39 @@
  * Created by ubuntu on 3/26/16.
  */
 exports.posList2Key = function (posList) {
-    var key = "";
-    for(var i = 0;i<posList.length;i++) {
-        key += pos2Char(posList[i]);
+    let newPosList = rearrange(posList);
+    let key = "";
+    for(let i = 0;i<newPosList.length;i++) {
+        key += pos2Char(newPosList[i]);
     }
 
     return key;
+
+    // rearrange posList, to avoid duplicate positions
+    function rearrange(posList) {
+        let oddList = [];
+        let evenList = [];
+        let resultList = [];
+        for(let i = 0;i<posList.length;i++) {
+            if(i%2==0) {
+                oddList.push(posList[i]);
+            } else {
+                evenList.push(posList[i]);
+            }
+        }
+    
+        oddList.sort((a,b)=>a-b);
+        evenList.sort((a,b)=>a-b);
+    
+        for(let i = 0;i<posList.length;i++) {
+            if(i%2==0) {
+                resultList.push(oddList.shift());
+            } else {
+                resultList.push(evenList.shift());
+            }
+        }
+        return resultList;
+    }
 
     function pos2Char(pos) {
         pos = pos & 0xff;
@@ -16,22 +43,12 @@ exports.posList2Key = function (posList) {
     }
 };
 
-exports.key2PosList = function(data) {
-    var array = [];
-
-    for(var i = 0;i<data.length;i++) {
-        var pos = data.charCodeAt(i);
-        array.push(pos);
-    }
-    return array;
-};
-
 exports.combineTypeLevel = function(type, level) {
     return ''+type+':'+level;
 };
 
 exports.extractLevel = function(typeLevel) {
-    var array = typeLevel.split(":");
+    let array = typeLevel.split(":");
     return {
         type: parseInt(array[0]),
         level: parseInt(array[1])
