213ed5951924e54d6e58789413f1ed65b1ef0a52
hillwater <hillwater.zju@gmail.com>
2023-10-25 10:34:17 +0800

    change autoPlayGame.sh

--- autoPlayGame.sh
+++ autoPlayGame.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-node autoPlayGame.js slave > autoPlayGameOutput.log 2>&1 &
+node autoPlayGame.js slave > autoPlayGameOutput1.log 2>&1 &
+node autoPlayGame.js slave > autoPlayGameOutput2.log 2>&1 &
